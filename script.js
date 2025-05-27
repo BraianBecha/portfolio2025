130054e342009d03217efabab8ef9582c68500d3
BraianBecha <braian.becha.hace@gmail.com>
2025-05-27 00:12:13 -0300

    oculto section2 al principio

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ console.log(menu)
 console.log(menu.style.display)
 menu.style.display="none";
 
-var setion1 = document.getElementById("section1");
-var setion2 = document.getElementById("section2");
+var section1 = document.getElementById("section1");
+var section2 = document.getElementById("section2");
 var saludo1= document.getElementById("saludo1")
 var sideCard = document.getElementById("side-card")
+
+section2.style.display="none";
 
 var card = document.querySelectorAll(".card");
 
@@ -40,16 +42,16 @@ flag=false;
 function mostrarPrincipal(){
     dropDown();
     console.log("llamada a principal");
-    setion2.style.display="none";
-    setion1.style.display="block";
+    section2.style.display="none";
+    section1.style.display="block";
     saludo1.style.display="block";    
     sideCard.style.display="block";
 };
 function mostrarAbout(){
     dropDown();
     console.log("llamada a about");
-    setion1.style.display="none";
-    setion2.style.display="block";
+    section1.style.display="none";
+    section2.style.display="block";
     saludo1.style.display="none";
     sideCard.style.display="none";
 };
